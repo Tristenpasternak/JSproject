b75b00d7a5ddb0c21ce357828644800916f0b5d0
Tristen Pasternak <tjp2153@barnard.com>
2019-04-24 20:13:56 -0400

    working program, needs cookies

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('hello')
+new Vue ({
+ el: "#app",
+ data: {
+ 	user: "Tristen",
+ 	todos: [
+ 		//{text: "Practice Trumpet", value: 2, goal: 7},
+ 		//{text: "Meditate", value: 3, goal: 4},
+ 		//{text: "Study Bio", value: 1, goal: 5},
+ 	],
+ 	newTodo: "",
+ 	newGoal: "",
+
+ },
+ methods: {
+ 	add:function(){
+    	this.todos.push({text: this.newTodo, value:0, goal: this.newGoal, last: new Date()})
+    	this.newTodo=""
+	},
+	checkTime:function(e, todo){
+		
+		//todo.last
+		var now = new Date();
+
+		var timeDiff = (now - todo.last)/1000 ;
+	
+		if(timeDiff >= 86400) {
+
+			todo.value++;
+			todo.last = now;
+		} else {
+			console.log('cant update yet')
+		}
+	}
+
+	// value:function(){
+	// 	if(timeDiff) >= 86400
+	// }
+	}
+})
